a2b7c1f83ad3632e434df937c8e227f0ef50732b
matheusg18 <matheusgabriel.18@gmail.com>
2022-01-03 18:49:31 -0300

    Estiliza as informações do produto

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,26 +5,28 @@ function Product() {
   return (
     <main>
       <MobileProductImages />
-      <h2>Sneaker Company</h2>
-      <h3>Fall Limited Edition Sneakers</h3>
-      <p>
-        These low-profile sneakers are your perfect casual wear companion. Featuring a durable
-        rubber outer sole, they'll withstand everything the weather can offer.
-      </p>
-      <div>
-        <span>$125.00</span>
-        <span>50%</span>
-        <span>$250.00</span>
-      </div>
-      <div>
-        <img src="images/icon-minus.svg" alt="decrease quantity" />
-        <span>0</span>
-        <img src="images/icon-plus.svg" alt="increase quantity" />
-      </div>
-      <button>
-        <img src="images/icon-cart.svg" alt="add to cart" />
-        <span>Add to cart</span>
-      </button>
+      <section className="product">
+        <h2 className="product-brand">Sneaker Company</h2>
+        <h3 className="product-name">Fall Limited Edition Sneakers</h3>
+        <p className="product-description">
+          These low-profile sneakers are your perfect casual wear companion. Featuring a durable
+          rubber outer sole, they'll withstand everything the weather can offer.
+        </p>
+        <div className="product-price">
+          <span className="current-price">$125.00</span>
+          <span className="discount">50%</span>
+          <span className="original-price">$250.00</span>
+        </div>
+        <div className="product-quantity">
+          <img src="images/icon-minus.svg" alt="decrease quantity" />
+          <span>0</span>
+          <img src="images/icon-plus.svg" alt="increase quantity" />
+        </div>
+        <button className="product-buy-button">
+          <img src="images/icon-cart.svg" alt="add to cart" />
+          <span>Add to cart</span>
+        </button>
+      </section>
     </main>
   );
 }
